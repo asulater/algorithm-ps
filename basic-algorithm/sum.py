86040506c79d2d007599e1324e323e43702195a6
asulater <asulater@gmail.com>
2021-12-07 23:26:38 +0900

    updates

--- basic-algorithm/sum.py
+++ basic-algorithm/sum.py
@@ -1,4 +1,4 @@
-# 1st Find the sum from 1 to n.
+# 1) Find the sum from 1 to n.
 
 def sum(n):
     return n * (n+1) // 2 # // = Integer
@@ -7,12 +7,16 @@ def sum(n):
 
 
 
-# The sum of squares of consecutive numbers from 1 to n.
+# 1-1) The sum of squares of consecutive numbers from 1 to n.
 
-def sum(n):
-    res = 0
-    for i in range(1, n+1):
-       res += i ** 2
-    return res
+# def sum_sq(n):
+#     res = 0
+#     for i in range(1, n+1):
+#        res += i ** 2
+#     return res
 
-print(sum(10)) # result
+def sum_sq(n):
+    return n * (n+1) * (2 * n +1) // 6
+
+print(sum_sq(100)) # result
+
