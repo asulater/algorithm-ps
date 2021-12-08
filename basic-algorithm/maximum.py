d7c29a1215cffd1ba67a75f15ca5602d8b125360
asulater <asulater@gmail.com>
2021-12-08 22:32:40 +0900

    updates

--- basic-algorithm/maximum.py
+++ basic-algorithm/maximum.py
@@ -1,11 +1,27 @@
-def find_max(a):
-    n = len(a)
-    max_v = a[0]
-    for i in range(1, n):
-        if a[i] > max_v:
-            max_v = a[i]
-    return max_v
+# 2) Find the maximum value of the list.
+
+# def find_max(a):
+#     n = len(a)
+#     max_v = a[0]
+#     for i in range(1, n):
+#         if a[i] > max_v:
+#             max_v = a[i]
+#     return max_v
+             
 
 v = [17, 92, 18, 33, 58, 7, 33, 42]
 
-print(find_max(v))
+# print(find_max(v))
+
+
+# 2-1) Find the location of the maximum value
+
+def find_max_idx(a):
+    n = len(a)
+    max_idx = 0
+    for i in range(1, n):
+        if a[i] > a[max_idx]:
+            max_idx = i
+    return max_idx
+
+print(find_max_idx(v))
