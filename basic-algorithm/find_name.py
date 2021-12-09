503bbe05c889e57090d22ce8fb1092b22b95d4fb
asulater <asulater@gmail.com>
2021-12-09 21:18:14 +0900

    updates

--- basic-algorithm/find_name.py
+++ basic-algorithm/find_name.py
@@ -1,15 +1,20 @@
 # 3) Find the same name among people and make it into a set and return
 
-def find_same_name(a):
-    n = len(a)
-    res = set()
-    for i in range(0, n-1):
-        for j in range(i + 1, n):
-            if a[i] == a[j]:
-                res.add(a[i])
-    return res
+# def find_same_name(a):
+#     n = len(a)
+#     res = set()
+#     for i in range(0, n-1):
+#         for j in range(i + 1, n):
+#             if a[i] == a[j]:
+#                 res.add(a[i])
+#     return res
 
 
-name = ["Tom", "Jerry", "Mike", "Tom"]
+# name = ["Tom", "Jerry", "Mike", "Tom"]
 
-print(find_same_name(name))
+# print(find_same_name(name))
+
+
+
+# 3-1) Pick two out of n and mate. Print out all the combinations you can pair up with
+
