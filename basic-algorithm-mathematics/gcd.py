8f9a3c3cf72818a2c52a1499be5a925dfb0eec23
asulater <asulater@gmail.com>
2021-12-11 19:50:43 +0900

    updates

---
+++ basic-algorithm-mathematics/gcd.py
@@ -0,0 +1,17 @@
+# 5) Greatest Common Divisor
+
+def gcd(a, b):
+    i = min(a, b) # Find the minimum value out of the two numbers
+    while True:
+        if a % i == 0  and b % i == 0 :
+            return i
+        i = i - 1
+
+print(gcd(1, 5))
+print(gcd(3, 6))
+print(gcd(60, 24))
+
+
+
+# 5-1) Euclid algorithm
+
