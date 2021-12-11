c9bb0e7e589dc58cc70e7c5a3e2072bf0ec2e6d0
asulater <asulater@gmail.com>
2021-12-11 22:31:34 +0900

    updates

--- basic-algorithm-mathematics/sequential.py
+++ basic-algorithm-mathematics/sequential.py
@@ -1,6 +1,6 @@
 # 7) sequential search
 
-v = [17, 92, 18, 33, 58, 7, 33, 42] # List
+v = [17, 92, 18, 33, 58, 7, 33] # List
 
 def search_list(a, x):
     n = len(a)
@@ -12,3 +12,32 @@ def search_list(a, x):
 print(search_list(v, 18))
 print(search_list(v, 33))
 print(search_list(v, 900))
+
+# 7-1) 
+
+def search_list2(a, x):
+    n = len(a)
+    temp = []
+    for i in range(0, n):
+        if x == a[i]:
+            temp.append(i)
+    return temp
+
+print(search_list2(v, 18))
+print(search_list2(v, 900))
+
+
+# 7-2) 
+
+stu_no = [39, 14, 67, 105]
+stu_name  = ["Justin", "John", "Mike", "Summer"]
+
+def stu_search(a, b, c):
+    for i in range(0, len(b)):
+        if a == b[i]:
+            return c[i]
+    return "?"
+
+print(stu_search(39, stu_no, stu_name))
+print(stu_search(900, stu_no, stu_name))
+print(stu_search(105, stu_no, stu_name))
