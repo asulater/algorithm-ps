31828f3a902377bf6e1040a1315183fde587e462
asulater <asulater@gmail.com>
2021-12-12 14:31:00 +0900

    updates

--- basic-algorithm-mathematics/selection.py
+++ basic-algorithm-mathematics/selection.py
@@ -1,2 +1,34 @@
 # 8) Selection Sort
 
+d = [2, 4, 5, 1, 3]
+
+# def find_min_idx(a):
+#     n = len(a)
+#     min_idx = 0
+#     for i in range(1, n):
+#         if a[i] < a[min_idx]:
+#             min_idx = i
+#     return min_idx
+
+# def sel_sort(a):
+#     result = []
+#     while a:
+#         min_idx = find_min_idx(a)
+#         value = a.pop(min_idx)
+#         result.append(value)
+#     return result
+
+# print(sel_sort(d))
+
+
+def sel_sort(a):
+    n = len(a)
+    for i in range(0, n - 1):
+        min_idx = i
+        for j in range(i + 1, n):
+            if a[j] < a[min_idx]:
+                min_idx = j
+                a[i], a[min_idx] = a[min_idx], a[i]
+
+sel_sort(d)
+print(d)
