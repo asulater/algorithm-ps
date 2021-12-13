e73d99fe31b8adecb7a1abc2ebd0ad19af8daba7
asulater <asulater@gmail.com>
2021-12-13 21:29:24 +0900

    updates

--- basic-algorithm-mathematics/bsearch.py
+++ basic-algorithm-mathematics/bsearch.py
@@ -24,5 +24,22 @@ print(binary_search(d, 50))
 
 # 13-1) Binary Search (use recursive call)
 
+def binary_search_sub(a, x, start, end):
+    if start > end:
+        return -1
+    
+    mid = (start + end) // 2
+    if x == a[mid]:
+        return mid
+    elif x > a[mid]:
+        return binary_search_sub(a, x, mid + 1, end)
+    else:
+        return binary_search_sub(a, x, start, mid - 1)
+    return -1
 
+def b_search(a, x):
+    return binary_search_sub(a, x, 0, len(a) - 1)
+
+print(b_search(d, 36))
+print(b_search(d, 50))
 
