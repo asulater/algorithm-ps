9f322f7925e6f952596279bae13d3b86b333023e
asulater <asulater@gmail.com>
2021-12-19 23:06:57 +0900

    updates

--- basic-algorithm-mathematics/graph.py
+++ basic-algorithm-mathematics/graph.py
@@ -1,2 +1,13 @@
 # 16) graph
 
+fr_info = {
+    'Summer': ['John', 'Justin', 'Mike'],
+    'John': ['Summer', 'Justin'],
+    'Justin': ['John', 'Summer', 'Mike', 'May'],
+    'Mike': ['Summer', 'Justin'],
+    'May': ['Justin', 'Kim'],
+    'Kim': ['May'],
+    'Tom': ['Jerry'],
+    'Jerry': ['Tom']
+}
+
