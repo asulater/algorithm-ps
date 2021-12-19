288c2798c445e89d70eed4cdf1e26dacf3ef6029
asulater <asulater@gmail.com>
2021-12-19 23:26:09 +0900

    updates

--- basic-algorithm-mathematics/graph.py
+++ basic-algorithm-mathematics/graph.py
@@ -15,15 +15,15 @@ def print_all_friends(g, start):
     qu = []
     done = set()
 
-    qu.append(start)
+    qu.append((start, 0))
     done.add(start)
 
     while qu:
-        p = qu.pop(0)
-        print(p)
+        (p, d) = qu.pop(0)
+        print(p, d)
         for x in g[p]:
             if x not in done:
-                qu.append(x)
+                qu.append((x, d + 1))
                 done.add(x)
 
 print_all_friends(fr_info, 'Summer')
